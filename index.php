be649bf5806f6323a3da9c8aed76a79f80f96b85
AzatFMS <kasper_pnv@mail.ru>
2012-01-31 12:34:46 +0400

    1

--- index.php
+++ index.php
@@ -1,11 +1,10 @@
 <?php
 
 include 'core/core.php';
-
 DB_Provider::Instance()->loadProvider('Core');
 
 add_file('views/views_core.php', __FILE__);
 
 new Template(env::vars()->PAGE);
 
-// Logger::print_log();
+Logger::print_log();
